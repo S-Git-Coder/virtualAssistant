4d9723a31c2a06652c39318fc32834be3d396075
Sahilkumar Patel <sahilpatel9313@gmail.com>
2025-08-03 00:36:29 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -61,8 +61,8 @@ export const Login = async (req, res) => {
         res.cookie("token", token,{
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
 
         return res.status(200).json(user);
