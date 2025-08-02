aa0df09ee9516ccb3aa94a9dd9971a7a6caab457
Sahilkumar Patel <sahilpatel9313@gmail.com>
2025-08-03 01:02:18 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -74,7 +74,10 @@ export const Login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
+        res.clearCookie("token", {
+  sameSite: "None",
+  secure: true
+})
         return res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
         return res.status(500).json({ message: `Logout error : ${error}` });
