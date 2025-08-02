1e28824b067d3a14cfc4c4b8999ac01fcc54caa3
Sahilkumar Patel <sahilpatel9313@gmail.com>
2025-08-03 00:33:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from './gemini.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtualassistant-3wmu.onrender.com",
     credentials: true
 }))
 app.use(express.json())
